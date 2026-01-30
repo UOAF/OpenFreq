c154cf71aa4e6ae7ae1a89545164be26438dbc98
richionizor <7107174+richionizor@users.noreply.github.com>
2026-01-30 09:56:00 +0100

    styling

---
+++ OpenFreq.Client/Converters/BoolToGreenGrayConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace OpenFreqClient.Converters
+{
+    /// <summary>
+    /// Converts a boolean value to Green (true) or Gray (false) color brush
+    /// </summary>
+    public class BoolToGreenGrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && boolValue)
+            {
+                return new SolidColorBrush(Color.FromRgb(76, 175, 80)); // Material Green 500
+            }
+            
+            return new SolidColorBrush(Color.FromRgb(158, 158, 158)); // Material Gray 500
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("BoolToGreenGrayConverter does not support ConvertBack");
+        }
+    }
+}
---
+++ OpenFreq.Client/Converters/HotkeyTooltipConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace OpenFreqClient.Converters
+{
+    /// <summary>
+    /// Converts a boolean indicating whether a hotkey is set to an appropriate tooltip string
+    /// </summary>
+    public class HotkeyTooltipConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool hasHotkey)
+            {
+                return hasHotkey 
+                    ? "Click to change hotkey (or clear it)" 
+                    : "Click to set a hotkey for Push-To-Talk";
+            }
+            
+            return "Set hotkey";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("HotkeyTooltipConverter does not support ConvertBack");
+        }
+    }
+}
