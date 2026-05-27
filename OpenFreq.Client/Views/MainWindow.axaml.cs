db2e6eea23c5aa30e7223ce8d4f894193ffbbc20
richionizor <7107174+richionizor@users.noreply.github.com>
2026-05-27 20:27:52 +0200

    adds confirmation dialog when exiting and connected in BMS mode (#62)

--- OpenFreq.Client/Views/MainWindow.axaml.cs
+++ OpenFreq.Client/Views/MainWindow.axaml.cs
@@ -1,13 +1,19 @@
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
+using OpenFreqClient.Services.Interfaces;
 using OpenFreqClient.ViewModels;
+using OpenFreqClient.Views.Util;
 
 namespace OpenFreqClient.Views;
 
 public partial class MainWindow : Window
 {
     private MainWindowViewModel? _viewModel;
+    
+    // For confirmation dialog if BMS mode set and connected
+    private bool _forceClose ;
 
     public MainWindow()
     {
@@ -27,10 +33,10 @@ public partial class MainWindow : Window
         {
             Title = "Open Heightmap File",
             AllowMultiple = false,
-            FileTypeFilter = new[]
-            {
+            FileTypeFilter =
+            [
                 new FilePickerFileType("BMS NT HeightMap") { Patterns = new[] { "HeightMap.raw" } }
-            }
+            ]
         };
 
         var file = await storage.OpenFilePickerAsync(filepickerOptions);
@@ -39,9 +45,32 @@ public partial class MainWindow : Window
             _viewModel?.Settings.HeightmapPath = file[0].Path.LocalPath;
         }
     }
-    
+
     protected override void OnClosing(WindowClosingEventArgs e)
     {
         _viewModel?.Settings.UpdateWindowSettings();
+
+        if (!_forceClose &&
+            _viewModel?.Settings.ConnectionMode == IOpenFreqService.Mode.BMS &&
+            _viewModel.OpenFreqConnected)
+        {
+            e.Cancel = true;
+            _ = ConfirmExitAsync();
+        }
+    }
+
+    private async Task ConfirmExitAsync()
+    {
+        var confirmed = await ConfirmationDialogService.ShowAsync(
+            "Exit OpenFreq",
+            "Connected in BMS mode. Exit anyway?",
+            "Cancel",
+            "Exit");
+
+        if (confirmed)
+        {
+            _forceClose = true;
+            Close();
+        }
     }
 }
